c99d5dc417f637f49bfd399b4b6e9e92940cf519
Gregory <strogg1987@gmail.com>
2023-10-27 23:51:50 +0700

    RTOS+ADC

--- src/app.c
+++ src/app.c
@@ -4,11 +4,14 @@
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/cm3/systick.h>
 #include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/adc.h>
+#include "FreeRTOS.h"
+#include "task.h"
 // private variables and prototypes
 volatile uint32_t sys_millis;
 void config_timer16(void);
 void config_timer17(void);
-
+uint8_t channel_array[] = {1, 1, ADC_CHANNEL_TEMP};
 const struct rcc_clock_scale rcc_clock_scale_hse_8mhz = {
     /* 64mhz from hse@8mhz via pll @ 128mhz / 2, scale1, 2ws */
     .sysclock_source = RCC_PLL,
@@ -25,40 +28,41 @@ const struct rcc_clock_scale rcc_clock_scale_hse_8mhz = {
     .ahb_frequency = 64000000,
     .apb_frequency = 64000000,
 };
-
-void sys_tick_handler(void)
+void vApplicationStackOverflowHook(
+    TaskHandle_t xTask __attribute__((unused)),
+    char *pcTaskName __attribute__((unused)))
 {
-    sys_millis++;
-}
 
-static void init_systick(void)
-{
-    systick_set_reload(64000);
-    systick_set_clocksource(STK_CSR_CLKSOURCE_AHB);
-    systick_counter_enable();
-    systick_interrupt_enable();
-}
-
-static void delay_ms(uint32_t ms)
-{
-    uint32_t wake = sys_millis + ms;
-    while (wake > sys_millis)
+    for (;;)
         ;
+}
+
+static void task1(void *args __attribute__((unused)))
+{
+    for (;;)
+    {
+        adc_start_conversion_regular(ADC1);
+        while (!(adc_eoc(ADC1)))
+            ;
+        volatile uint16_t temp = adc_read_regular(ADC1);
+        gpio_toggle(GPIOC, GPIO8);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
 }
 
 void config_timer16(void)
 {
     rcc_periph_clock_enable(RCC_TIM16);
-    timer_set_prescaler(TIM16, 7);           // s/b 8MHz
-    timer_enable_preload(TIM16);             // causes counter to be loaded from its ARR only at next update event
-    timer_set_period(TIM16, 1000);           // set the timer period in the (ARR) auto-reload register
-    timer_set_oc_value(TIM16, TIM_OC1, 250); // set duty cycle to 25%
-    timer_set_counter(TIM16, 0);             // TIM_CNT
+    timer_set_prescaler(TIM16, 7);
+    timer_enable_preload(TIM16);
+    timer_set_period(TIM16, 1000);
+    timer_set_oc_value(TIM16, TIM_OC1, 250);
+    timer_set_counter(TIM16, 0);
     timer_enable_oc_preload(TIM16, TIM_OC1);
-    timer_set_oc_mode(TIM16, TIM_OC1, TIM_OCM_PWM1); // output active when counter is lt compare register
-    timer_enable_oc_output(TIM16, TIM_OC1);          // enable timer output compare
-    timer_continuous_mode(TIM16);                    // enable the timer to run continuously
-    timer_generate_event(TIM16, TIM_EGR_UG);         // required!
+    timer_set_oc_mode(TIM16, TIM_OC1, TIM_OCM_PWM1);
+    timer_enable_oc_output(TIM16, TIM_OC1);
+    timer_continuous_mode(TIM16);
+    timer_generate_event(TIM16, TIM_EGR_UG);
     timer_enable_counter(TIM16);
     // timer_enable_irq(TIM16, TIM_DIER_COMIE);  //enable commutation interrupt
     // nvic_enable_irq(NVIC_TIM1_CC_IRQ);
@@ -80,18 +84,34 @@ void config_timer17(void)
     timer_enable_counter(TIM17);
 }
 
+static void adc_setup(void)
+{
+    rcc_periph_clock_enable(RCC_ADC);
+    adc_power_off(ADC1);
+    adc_set_clk_prescale(ADC1, ADC_CCR_PRESC_DIV2);
+    adc_set_single_conversion_mode(ADC1);
+    adc_set_right_aligned(ADC1);
+    adc_set_sample_time_on_all_channels(ADC1, ADC_SMPTIME_160DOT5);
+    uint8_t channel = 0;
+    adc_set_regular_sequence(ADC1, 1, &channel);
+    adc_set_resolution(ADC1, ADC_CFGR1_RES_12_BIT);
+    adc_power_on(ADC1);
+    adc_start_conversion_regular(ADC1);
+}
+
 int main(void)
 {
 
     rcc_clock_setup(&rcc_clock_scale_hse_8mhz);
-    init_systick();
     rcc_periph_clock_enable(RCC_GPIOC);
     config_timer16();
     config_timer17();
+    adc_setup();
     gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
+    // Tell FreeRTOS about our toggle task, and set it's stack and priority
+    xTaskCreate(task1, "LED", 100, NULL, 2, NULL);
+    vTaskStartScheduler();
     for (;;)
-    {
-        gpio_toggle(GPIOC, GPIO8);
-        delay_ms(100);
-    }
+        ;
+    return 0;
 }
