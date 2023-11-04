75257c539368b992a2f7b92d285d3b1916967a11
Gregory <strogg1987@gmail.com>
2023-11-04 19:41:53 +0700

    working usart

--- src/app.c
+++ src/app.c
@@ -8,11 +8,24 @@
 #include <libopencm3/stm32/usart.h>
 #include "FreeRTOS.h"
 #include "task.h"
+#include <memory.h>
+#include "nanomodbus.h"
+
+#define COILS_ADDR_MAX 16
+#define REGS_ADDR_MAX 32
+#define RTU_SERVER_ADDRESS 1
 // private variables and prototypes
 volatile uint32_t sys_millis;
-void config_timer16(void);
-void config_timer17(void);
 uint8_t channel_array[] = {1, 1, ADC_CHANNEL_TEMP};
+typedef struct
+{
+    char data[50];
+    uint8_t len;
+} buffer;
+
+buffer msg;
+buffer rx_buffer;
+
 const struct rcc_clock_scale rcc_clock_scale_hse_8mhz = {
     /* 64mhz from hse@8mhz via pll @ 128mhz / 2, scale1, 2ws */
     .sysclock_source = RCC_PLL,
@@ -40,13 +53,9 @@ void vApplicationStackOverflowHook(
 
 static void task1(void *args __attribute__((unused)))
 {
-    volatile uint8_t local = 0;
     for (;;)
     {
-        adc_start_conversion_regular(ADC1);
-        while (!(adc_eoc(ADC1)))
-            ;
-        volatile uint16_t temp = adc_read_regular(ADC1);
+        usart_send_blocking(USART1, '0');
         gpio_toggle(GPIOC, GPIO8);
         vTaskDelay(pdMS_TO_TICKS(500));
     }
@@ -54,65 +63,44 @@ static void task1(void *args __attribute__((unused)))
 
 static void usart_setup(void)
 {
-    /* Setup USART parameters. */
-    usart_set_baudrate(USART1, 115200);
+    nvic_enable_irq(NVIC_USART1_IRQ);
+    rcc_periph_clock_enable(RCC_USART1);
+    rcc_periph_clock_enable(RCC_GPIOC);
+    // gpio_mode_setup(GPIOC, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5|GPIO4);
+    // gpio_set_af(GPIOC, GPIO_AF0, GPIO5|GPIO4);
+
+    rcc_periph_clock_enable(RCC_GPIOB);
+    gpio_mode_setup(GPIOB, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO6 | GPIO7);
+    gpio_set_af(GPIOB, GPIO_AF0, GPIO6 | GPIO7);
+
+    usart_set_baudrate(USART1, 9600);
     usart_set_databits(USART1, 8);
     usart_set_parity(USART1, USART_PARITY_NONE);
-    usart_set_stopbits(USART1, USART_CR2_STOPBITS_1);
-    usart_set_mode(USART1, USART_MODE_TX);
+    usart_set_stopbits(USART1, USART_STOPBITS_1);
+    usart_set_mode(USART1, USART_MODE_TX_RX);
     usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+    usart_set_rx_timeout_value(USART1, 22);
+    usart_enable_rx_timeout_interrupt(USART1);
 
-    /* Finally enable the USART. */
+    usart_enable_rx_interrupt(USART1);
     usart_enable(USART1);
 }
 
-void config_timer16(void)
+void usart1_isr()
 {
-    rcc_periph_clock_enable(RCC_TIM16);
-    timer_set_prescaler(TIM16, 7);
-    timer_enable_preload(TIM16);
-    timer_set_period(TIM16, 1000);
-    timer_set_oc_value(TIM16, TIM_OC1, 250);
-    timer_set_counter(TIM16, 0);
-    timer_enable_oc_preload(TIM16, TIM_OC1);
-    timer_set_oc_mode(TIM16, TIM_OC1, TIM_OCM_PWM1);
-    timer_enable_oc_output(TIM16, TIM_OC1);
-    timer_continuous_mode(TIM16);
-    timer_generate_event(TIM16, TIM_EGR_UG);
-    timer_enable_counter(TIM16);
-    // timer_enable_irq(TIM16, TIM_DIER_COMIE);  //enable commutation interrupt
-    // nvic_enable_irq(NVIC_TIM1_CC_IRQ);
-}
-
-void config_timer17(void)
-{
-    rcc_periph_clock_enable(RCC_TIM17); // enable
-    timer_set_prescaler(TIM17, 7);
-    timer_enable_preload(TIM17);
-    timer_set_period(TIM17, 1000);
-    timer_set_oc_value(TIM17, TIM_OC1, 250);
-    timer_set_counter(TIM17, 0);
-    timer_enable_oc_preload(TIM17, TIM_OC1);
-    timer_set_oc_mode(TIM17, TIM_OC1, TIM_OCM_PWM1);
-    timer_enable_oc_output(TIM17, TIM_OC1);
-    timer_continuous_mode(TIM17);
-    timer_generate_event(TIM17, TIM_EGR_UG);
-    timer_enable_counter(TIM17);
-}
-
-static void adc_setup(void)
-{
-    rcc_periph_clock_enable(RCC_ADC);
-    adc_power_off(ADC1);
-    adc_set_clk_prescale(ADC1, ADC_CCR_PRESC_DIV2);
-    adc_set_single_conversion_mode(ADC1);
-    adc_set_right_aligned(ADC1);
-    adc_set_sample_time_on_all_channels(ADC1, ADC_SMPTIME_160DOT5);
-    uint8_t channel = 0;
-    adc_set_regular_sequence(ADC1, 1, &channel);
-    adc_set_resolution(ADC1, ADC_CFGR1_RES_12_BIT);
-    adc_power_on(ADC1);
-    adc_start_conversion_regular(ADC1);
+    if (((USART_CR1(USART1) & USART_CR1_RXNEIE) != 0) &&
+        ((USART_ISR(USART1) & USART_ISR_RXNE) != 0))
+    {
+        rx_buffer.data[rx_buffer.len] = usart_recv(USART1);
+        rx_buffer.len++;
+    }
+    if ((USART_CR1(USART1) & USART_CR1_RTOIE) != 0)
+    {
+        usart_disable_rx_interrupt(USART1);
+        memcpy(msg.data, rx_buffer.data, rx_buffer.len);
+        msg.len = rx_buffer.len;
+        rx_buffer.len = 0;
+    }
 }
 
 int main(void)
@@ -120,11 +108,9 @@ int main(void)
 
     rcc_clock_setup(&rcc_clock_scale_hse_8mhz);
     rcc_periph_clock_enable(RCC_GPIOC);
-    config_timer16();
-    config_timer17();
-    adc_setup();
+    usart_setup();
+    rx_buffer.len = 0;
     gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
-    // Tell FreeRTOS about our toggle task, and set it's stack and priority
     xTaskCreate(task1, "LED", 100, NULL, 2, NULL);
     vTaskStartScheduler();
     for (;;)
